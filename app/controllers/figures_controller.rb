fb9d2fa2922b2964432d7caee214ebf490eb9e46
WilliamMena <williammena1991@gmail.com>
2016-09-25 22:20:38 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,13 +12,24 @@ class FiguresController < ApplicationController
 
   post '/figures' do 
     f = Figure.find_or_create_by(name: params["figure"]["name"])
-    params["figure"]["title_ids"].each do |t|
-      f.titles << Title.find_by_id(t)
+
+    if params["title"]["name"].empty?
+      params["figure"]["title_ids"].each do |t|
+        f.titles << Title.find_by_id(t)
+      end
+    else
+      f.titles = Title.find_or_create_by(name: params["title"]["name"])
     end
-    binding.pry
-    params["figure"]["landmark_ids"].each do |l|
-      f.landmarks << Landmark.find_by_id(l)
+
+
+    if params["figure"]["landmark_ids"]
+      params["figure"]["landmark_ids"].each do |l|
+        f.landmarks << Landmark.find_by_id(l)
+      end
+    else
+      f.landmarks << Landmark.find_or_create_by(params["landmark"]["name"])
     end
+
     f.save
   end
 
